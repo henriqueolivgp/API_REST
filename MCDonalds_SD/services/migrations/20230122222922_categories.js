fdf4198cdc2ca59a19745318f9711cb6b184e215
henriqueolivgp <henrique.o@ipvc.pt>
2023-01-22 23:48:50 +0000

    ...

--- MCDonalds_SD/services/migrations/20230122222922_categories.js
+++ MCDonalds_SD/services/migrations/20230122222922_categories.js
@@ -4,8 +4,8 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('categories', function(table) {
-        table.increments('id').primary();
-        table.string('category').notNullable()
+        table.increments('id');
+        table.string('category').primary().notNullable()
         table.timestamps(false, true);
       });
 };
