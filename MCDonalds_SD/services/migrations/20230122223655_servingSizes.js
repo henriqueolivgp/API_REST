323d0ba8e12b2ac485e25f83bfedb9b6071b7adc
henriqueolivgp <henrique.o@ipvc.pt>
2023-01-22 23:32:15 +0000

    ...

--- MCDonalds_SD/services/migrations/20230122223655_servingSizes.js
+++ MCDonalds_SD/services/migrations/20230122223655_servingSizes.js
@@ -3,11 +3,10 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.createTable('Items', function(table) {
-        table.increments('id').primary();
+    return knex.schema.createTable('ServingSizes', function(table) {
+        table.increments('id')
+        table.integer('servingsize').primary();
         table.string('item').notNullable();
-        table.integer('calories').foreign().notNullable();
-        table.integer('servingSizes').foreign().notNullable();
         table.timestamps(false, true);
       });
 };
@@ -17,5 +16,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('ServingSizes');
 };
--- MCDonalds_SD/services/migrations/20230122231048_Items.js
+++ MCDonalds_SD/services/migrations/20230122231048_Items.js
@@ -6,11 +6,14 @@ exports.up = function(knex) {
     return knex.schema.createTable('Items', function(table) {
         table.increments('id').primary();
         table.string('item').notNullable();
-        table.integer('calories').foreign().notNullable();
-        table.integer('servingSizes').foreign().notNullable();
+        table.integer('calories').notNullable();
+        table.integer('servingsize').unique().unsigned();
+        table.foreign('servingsize').references('ServingSizes.servingsize')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
         table.timestamps(false, true);
       });
-};
+}; 
 
 /**
  * @param { import("knex").Knex } knex
